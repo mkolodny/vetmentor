fee88077c8bd5234af621767ee617891eeb54d2e
Michael Kolodny <michaelckolodny@gmail.com>
2013-10-13 12:30:53 -0400

    set up django auth

--- vetmentor/urls.py
+++ vetmentor/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import patterns, include
 from mentor.views import LandingView
+from django.contrib import admin
+
+admin.autodiscover()
+
 
 urlpatterns = patterns('',
-    (r'', LandingView.as_view()),
-    (r'^accounts/', include('allauth.urls')),
+    (r'^$', LandingView.as_view()),
+    #(r'^accounts/', include('allauth.urls')),
+    (r'^admin/', include(admin.site.urls)),
 )
