e4a541acefb11472630855d1db045e7199993754
Johan <johan2422@gmail.com>
2013-10-13 04:16:53 -0400

    updated landing page and base template with nav bar

--- vetmentor/urls.py
+++ vetmentor/urls.py
@@ -1,7 +1,15 @@
+import os.path
 from django.conf.urls import patterns, include
 from mentor.views import LandingView
 
+site_media = os.path.join(
+    os.path.dirname(__file__),'site_media'
+)
+
 urlpatterns = patterns('',
+    (r'^site_media/(?P<path>.*)$',
+     'django.views.static.serve',{'document_root':site_media}),
+
     (r'', LandingView.as_view()),
     (r'^accounts/', include('allauth.urls')),
 )
